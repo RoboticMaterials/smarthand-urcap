839ffdab88405a35bf30eae41f62417a7b2308b2
KalervoHyyppa <51681634+KalervoHyyppa@users.noreply.github.com>
2019-08-28 15:05:49 -0600

    Attempting fix on ProgramNodeContribution

--- com.roboticmaterials.smarthand/src/main/java/com/roboticmaterials/smarthand/impl/SmartHandProgramNodeContribution.java
+++ com.roboticmaterials.smarthand/src/main/java/com/roboticmaterials/smarthand/impl/SmartHandProgramNodeContribution.java
@@ -41,6 +41,44 @@ import com.roboticmaterials.smarthand.impl.SmartHandInstallationNodeContribution
 public class SmartHandProgramNodeContribution implements ProgramNodeContribution {
 
     private static final String FORCEOC_KEY = "forceOC";
+	private final ProgramAPI programAPI;
+	private final ProgramAPIProvider apiProvider;
+	private final SmartHandProgramNodeView view;
+	private final DataModel model;
+	private final UndoRedoManager undoRedoManager;
+	
+	private static final String OUTPUT_KEY = "command";
+	private static final String FORCEO_KEY = "forceO";
+	private static final String FORCEC_KEY = "forceC";
+	private static final String FORCEW_KEY = "forceW";
+	private static final String APERTURE_KEY = "aperture";
+	private static final String APERTUREVAR_KEY = "aperturevar";
+	//private static final String POSEOBJECT_KEY = "object";
+	private static final String INFOOBJECT_KEY = "object";
+	private static final String CARTWAYPOINT_KEY = "waypoint";
+	
+	private static final String SUCCESSVAR_KEY = "var_success";
+	private static final String TARGETVAR_KEY = "var_target";
+	private static final String APPROACHVAR_KEY = "var_approach";
+	private static final String WIDTHVAR_KEY = "var_width";
+	
+	private static String[] commands;
+	private static final String DEFAULT_OUTPUT = "Open Gripper";
+	private static final int DEFAULT_FORCE = 100;
+	private static final int DEFAULT_APERTURE = 108;
+	private static final String DEFAULT_APERTUREVAR = "<none>";
+	private static final String DEFAULT_OBJECT = "generic";
+	private static final String DEFAULT_WAYPOINT = "home";
+	
+	private static final String PLANEFILTERVAR_KEY = "var_planefilter";
+	private static final Boolean DEFAULT_PLANEFILTER = true;
+	
+	private String IPADDRESS;
+	private String STATUS = "offline";
+	//private Timer timer; 
+	
+	private final ScriptSender sender;
+	private final ScriptExporter exporter;
     
 
 
@@ -52,3 +90,32 @@ public class SmartHandProgramNodeContribution implements ProgramNodeContribution
         this.view = view;
         this.model = model;
         this.undoRedoManager = this.apiProvider.getProgramAPI().getUndoRedoManager();
+
+    }
+
+    @Override
+    public void openView(){
+
+    }
+
+    @Override
+    public void closeView(){
+
+    }
+
+    @Override
+    public String getTitle(){
+        return null;
+    }
+
+    @Override
+    public boolean isDefined() {
+        return false;
+    }
+
+    @Override
+    public void genereateScript(ScriptWriter writer) (
+        
+    )
+
+}
