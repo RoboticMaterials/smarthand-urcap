42b3576e4a5b6394fefe55ba9b12387e30e3bb0e
KalervoHyyppa <51681634+KalervoHyyppa@users.noreply.github.com>
2019-08-29 09:21:00 -0600

    Testing grid layout

--- com.roboticmaterials.smarthand/src/main/java/com/roboticmaterials/smarthand/impl/SmartHandProgramNodeView.java
+++ com.roboticmaterials.smarthand/src/main/java/com/roboticmaterials/smarthand/impl/SmartHandProgramNodeView.java
@@ -71,7 +71,7 @@ public class SmartHandProgramNodeView implements SwingProgramNodeView<SmartHandP
     @Override
     public void buildUI(JPanel panel, final ContributionProvider<SmartHandProgramNodeContribution> provider) {
 
-        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+        panel.setLayout(new GridLayout(5,2));
 
         //Panel for basic tasks
         panel.add(createDescription("Basic"));
