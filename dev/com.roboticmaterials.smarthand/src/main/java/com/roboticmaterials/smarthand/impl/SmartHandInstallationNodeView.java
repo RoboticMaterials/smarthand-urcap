fad2ce515af9d38b12c516a9218aab7fb87a7f50
Kalervo Hyyppa <kalervohyyppa@gmail.com>
2019-09-06 16:44:49 -0600

    Added connect button

--- dev/com.roboticmaterials.smarthand/src/main/java/com/roboticmaterials/smarthand/impl/SmartHandInstallationNodeView.java
+++ dev/com.roboticmaterials.smarthand/src/main/java/com/roboticmaterials/smarthand/impl/SmartHandInstallationNodeView.java
@@ -87,6 +87,7 @@ public class SmartHandInstallationNodeView implements SwingInstallationNodeView<
             }
         }));
 
+		jPanel.add(createSenderInitGripperButton(contribution));
         jPanel.add(createVerticalSpacing());
 		jPanel.add(createInfo("Open and close gripper:"));
 		jPanel.add(createVerticalSpacing());
@@ -209,7 +210,7 @@ public class SmartHandInstallationNodeView implements SwingInstallationNodeView<
 
     private Box createSenderInitGripperButton(final SmartHandInstallationNodeContribution contribution) {
         Box box = Box.createHorizontalBox();
-        box.setAlignmentX(Component.LEFT_ALIGNMENT);
+        //box.setAlignmentX(Component.LEFT_ALIGNMENT);
 
         initGripperButton.addActionListener(new ActionListener() {
             @Override
